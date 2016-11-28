1d9b7d48c543e390ab2693353f66ff6b809c7eaf
Andrew DeMaria <lostonamountain@gmail.com>
2016-11-27 19:51:32 -0700

    Migrate from Properties to Commons-Configuration
    - Added loading of properties file before database initialization
    
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

---
+++ libresonic-main/src/main/java/org/libresonic/player/spring/AdditionalPropertySourceConfigurer.java
@@ -0,0 +1,20 @@
+package org.libresonic.player.spring;
+
+import org.apache.commons.configuration2.ImmutableConfiguration;
+import org.libresonic.player.service.ApacheCommonsConfigurationService;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.core.env.PropertySource;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+
+public class AdditionalPropertySourceConfigurer implements
+        ApplicationContextInitializer<ConfigurableWebApplicationContext> {
+
+    public void initialize(ConfigurableWebApplicationContext ctx) {
+
+        ApacheCommonsConfigurationService configurationService = new ApacheCommonsConfigurationService();
+        ImmutableConfiguration snapshot = configurationService.getImmutableSnapshot();
+
+        PropertySource ps = new CommonsConfigurationPropertySource("libresonic-pre-init-configs", snapshot);
+        ctx.getEnvironment().getPropertySources().addLast(ps);
+    }
+}
