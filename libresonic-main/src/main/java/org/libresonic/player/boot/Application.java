2ad8ec005cd98c948378ebbc566593d11e7d07d0
Rémi Cocula <rcocula@gmail.com>
2016-12-11 22:39:13 +0100

    Add SpringBoot application.

---
+++ libresonic-main/src/main/java/org/libresonic/player/boot/Application.java
@@ -0,0 +1,23 @@
+package org.libresonic.player.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class (via @SpringBootApplication)
+        // we actually don't need to override this method.
+        return application;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
