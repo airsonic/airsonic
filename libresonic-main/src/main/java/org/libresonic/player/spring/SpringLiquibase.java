a097644e5fc18c94ae94ebe0231e72ca20c13bc6
Andrew DeMaria <lostonamountain@gmail.com>
2016-12-23 15:15:54 -0700

    Created a better workaround for https://liquibase.jira.com/browse/CORE-2966
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- libresonic-main/src/main/java/org/libresonic/player/spring/SpringLiquibase.java
+++ libresonic-main/src/main/java/org/libresonic/player/spring/SpringLiquibase.java
@@ -51,6 +51,7 @@ public class SpringLiquibase extends liquibase.integration.spring.SpringLiquibas
         if (StringUtils.trimToNull(this.defaultSchema) != null) {
             database.setDefaultSchemaName(this.defaultSchema);
         }
+        liquibase.precondition.PreconditionFactory.getInstance().register(DbmsVersionPrecondition.class);
         return database;
     }
 
