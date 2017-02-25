464e2825d57cf61c34bae90c34cdc9f5b9de053c
Andrew DeMaria <lostonamountain@gmail.com>
2017-02-25 16:08:13 -0700

    Removed unused import
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- libresonic-main/src/main/java/org/libresonic/player/controller/DownloadController.java
+++ libresonic-main/src/main/java/org/libresonic/player/controller/DownloadController.java
@@ -41,7 +41,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
