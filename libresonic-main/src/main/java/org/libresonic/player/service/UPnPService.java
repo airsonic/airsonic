8e99811fe08887b94ea1edcb99e8679378608d52
Andrew DeMaria <lostonamountain@gmail.com>
2017-03-27 20:13:44 -0600

    Use proper resource loading for Upnp Logo #320
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- libresonic-main/src/main/java/org/libresonic/player/service/UPnPService.java
+++ libresonic-main/src/main/java/org/libresonic/player/service/UPnPService.java
@@ -117,7 +117,7 @@ public class UPnPService {
                 new ModelDetails(serverName),
                 new DLNADoc[]{new DLNADoc("DMS", DLNADoc.Version.V1_5)}, null);
 
-        Icon icon = new Icon("image/png", 512, 512, 32, getClass().getResource("logo-512.png"));
+        Icon icon = new Icon("image/png", 512, 512, 32, "logo-512", getClass().getResourceAsStream("logo-512.png"));
 
         LocalService<FolderBasedContentDirectory> contentDirectoryservice = new AnnotationLocalServiceBinder().read(FolderBasedContentDirectory.class);
         contentDirectoryservice.setManager(new DefaultServiceManager<FolderBasedContentDirectory>(contentDirectoryservice) {
