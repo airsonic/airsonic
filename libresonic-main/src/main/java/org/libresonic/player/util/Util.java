7e66fefe39d6d198e06893b693a3e774c5506aea
Andrew DeMaria <lostonamountain@gmail.com>
2017-04-22 09:29:49 -0600

    Fix import
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- libresonic-main/src/main/java/org/libresonic/player/util/Util.java
+++ libresonic-main/src/main/java/org/libresonic/player/util/Util.java
@@ -19,8 +19,6 @@
  */
 package org.libresonic.player.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
