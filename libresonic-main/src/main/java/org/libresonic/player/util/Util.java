87ed5160492b936f926aa9db6f6d8f37657dfbf2
Andrew DeMaria <lostonamountain@gmail.com>
2017-05-04 18:15:56 -0600

    Fixed missing imports
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- libresonic-main/src/main/java/org/libresonic/player/util/Util.java
+++ libresonic-main/src/main/java/org/libresonic/player/util/Util.java
@@ -19,6 +19,8 @@
  */
 package org.libresonic.player.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
