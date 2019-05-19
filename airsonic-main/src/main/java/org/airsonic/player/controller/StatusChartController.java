b505c3e8282760d635bc05431785ac0a3d2c9656
jvoisin <julien.voisin@dustri.org>
2019-05-19 22:43:42 +0200

    Please Checkstyle

--- airsonic-main/src/main/java/org/airsonic/player/controller/StatusChartController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/StatusChartController.java
@@ -39,6 +39,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.awt.*;
 import java.util.Collections;
 import java.util.Date;
