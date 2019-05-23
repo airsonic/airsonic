b6cbeb1cb859c83165ada66aa800a5bcab5ab45a
Andrew DeMaria <lostonamountain@gmail.com>
2019-05-23 11:30:55 -0600

    Remove unused method
    Signed-off-by: Andrew DeMaria <lostonamountain@gmail.com>

--- airsonic-main/src/main/java/org/airsonic/player/controller/StreamController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/StreamController.java
@@ -326,28 +326,6 @@ public class StreamController {
         return null;
     }
 
-    private long getFileLength(TranscodingService.Parameters parameters) {
-        MediaFile file = parameters.getMediaFile();
-
-        if (!parameters.isDownsample() && !parameters.isTranscode()) {
-            return file.getFileSize();
-        }
-        Integer duration = file.getDurationSeconds();
-        Integer maxBitRate = parameters.getMaxBitRate();
-
-        if (duration == null) {
-            LOG.warn("Unknown duration for " + file + ". Unable to estimate transcoded size.");
-            return file.getFileSize();
-        }
-
-        if (maxBitRate == null) {
-            LOG.error("Unknown bit rate for " + file + ". Unable to estimate transcoded size.");
-            return file.getFileSize();
-        }
-
-        return duration * (long)maxBitRate * 1000L / 8L;
-    }
-
     @Nullable
     private HttpRange getRange(HttpServletRequest request, MediaFile file) {
 
