1318d774cf52a5e85f4580ba5039dd6a625d9cf7
jvoisin <julien.voisin@dustri.org>
2019-06-05 23:22:46 +0200

    Add a missing include

--- airsonic-main/src/main/java/org/airsonic/player/security/GlobalSecurityConfig.java
+++ airsonic-main/src/main/java/org/airsonic/player/security/GlobalSecurityConfig.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
