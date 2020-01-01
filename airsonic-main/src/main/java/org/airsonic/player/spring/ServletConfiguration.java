884efc53acd2feeff49539906cf1387a62e45b08
François-Xavier Thomas <fx.thomas@gmail.com>
2020-01-02 00:39:35 +0100

    Config: Enable suffix path matching
    Starting from Spring Boot 2.0, suffix path matching is not enabled by
    default anymore[1], so we must enable it manually.
    
    Both setUseSuffixPatternMatch and favorPathExtension calls allow URLs
    with extensions to be resolved to the same mapping as without extension.
    
    In Airsonic's case, this is necessary, because a lot of our mappings assume
    this behavior (for example "/home.view" URL to a "/home" mapping, or the
    entire Subsonic REST API controller).
    
    This fixes crashes when accessing almost all pages (which expect to use
    .view URLs), and also fixes the following REST API tests:
    
        [ERROR] Failures:
        [ERROR]   AirsonicRestApiJukeboxIntTest>AbstractAirsonicRestApiJukeboxIntTest.jukeboxStartActionTest:202->AbstractAirsonicRestApiJukeboxIntTest.performStartAction:282 Status expected:<200> but was:<404>
        [ERROR]   AirsonicRestApiJukeboxIntTest>AbstractAirsonicRestApiJukeboxIntTest.jukeboxStopActionTest:225->AbstractAirsonicRestApiJukeboxIntTest.performStartAction:282 Status expected:<200> but was:<404>
        [ERROR]   AirsonicRestApiJukeboxLegacyIntTest.jukeboxStartActionTest:48->AbstractAirsonicRestApiJukeboxIntTest.jukeboxStartActionTest:202->AbstractAirsonicRestApiJukeboxIntTest.performStartAction:282 Status expected:<200> but was:<404>
        [ERROR]   AirsonicRestApiJukeboxLegacyIntTest.jukeboxStopActionTest:56->AbstractAirsonicRestApiJukeboxIntTest.jukeboxStopActionTest:225->AbstractAirsonicRestApiJukeboxIntTest.performStartAction:282 Status expected:<200> but was:<404>
    
    [1] https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide#spring-mvc-path-matching-default-behavior-change

--- airsonic-main/src/main/java/org/airsonic/player/spring/ServletConfiguration.java
+++ airsonic-main/src/main/java/org/airsonic/player/spring/ServletConfiguration.java
@@ -4,6 +4,9 @@ import org.airsonic.player.controller.PodcastController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -11,7 +14,7 @@ import org.springframework.web.servlet.view.JstlView;
 import java.util.Properties;
 
 @Configuration
-public class ServletConfiguration {
+public class ServletConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public SimpleUrlHandlerMapping podcastUrlMapping(PodcastController podcastController) {
@@ -30,5 +33,27 @@ public class ServletConfiguration {
         resolver.setPrefix("/WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    // Both setUseSuffixPatternMatch and favorPathExtension calls allow URLs
+    // with extensions to be resolved to the same mapping as without extension.
+    //
+    // In Airsonic's case, this is necessary, because a lot of our mappings assume
+    // this behavior (for example "/home.view" URL to a "/home" mapping, or the
+    // entire Subsonic REST API controller).
+    //
+    // Starting from Spring Boot 2.0, this feature is not enabled by default anymore,
+    // so we must enable it manually.
+    //
+    // See: https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide#spring-mvc-path-matching-default-behavior-change
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(true);
     }
 }
