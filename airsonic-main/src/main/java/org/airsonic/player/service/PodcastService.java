airsonic/airsonic

commit c65fa1671bb8cdf2d715c69bc6a53d5be17a2cd2

update naming convention as per recommendations
Forced episode date format to be (YYYY-MM-DD), and added Airsonic-generated podcast ID to filename.

airsonic-main/src/main/java/org/airsonic/player/service/PodcastService.java
@@ -657,7 +657,8 @@ public class PodcastService {
 
         File channelDir = getChannelDirectory(channel);
 
-        String filename = channel.getTitle() + " - " + episode.getPublishDate().toString() + " - " + episode.getTitle();
+        String episodeDate = String.format("%tY-%tm-%td", episode.getPublishDate());
+        String filename = channel.getTitle() + " - " + episodeDate + " - " + episode.getId() + " - " + episode.getTitle();
         filename = filename.substring(0, Math.min(filename.length(), 146));
 
         filename = StringUtil.fileSystemSafe(filename);

commit b2a32996fbf538434cac83a78a24aee4f81a89f8

Fix date format used in podcast filename generation
This fixes a crash when downloading podcasts introduced by c65fa1671.

airsonic-main/src/main/java/org/airsonic/player/service/PodcastService.java
@@ -657,7 +657,8 @@ public class PodcastService {
 
         File channelDir = getChannelDirectory(channel);
 
-        String episodeDate = String.format("%tY-%tm-%td", episode.getPublishDate());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String episodeDate = simpleDateFormat.format(episode.getPublishDate());
         String filename = channel.getTitle() + " - " + episodeDate + " - " + episode.getId() + " - " + episode.getTitle();
         filename = filename.substring(0, Math.min(filename.length(), 146));
 